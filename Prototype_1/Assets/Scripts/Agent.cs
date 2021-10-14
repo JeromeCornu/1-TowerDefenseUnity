3af54e781d82177e707aa2ffc1acb24ad9c60d52
JeromeCornu <jeromecornu02@gmail.com>
2021-10-14 10:50:17 +0200

    Delete Agent.cs

--- Prototype_1/Assets/Scripts/Agent.cs
+++ Prototype_1/Assets/Scripts/Agent.cs
@@ -5,15 +5,15 @@ using UnityEngine.AI;
 
 public class Agent : MonoBehaviour
 {
-    private Transform goal;
+    //private Transform goal;
 
     // Start is called before the first frame update
     void Start()
     {
-        goal = GameObject.Find("Goal").GetComponent<Transform>();
+        /*goal = GameObject.Find("Goal").GetComponent<Transform>();
 
         NavMeshAgent agent = GetComponent<NavMeshAgent>();
-        agent.destination = goal.position;
+        agent.destination = goal.position;*/
     }
 
     // Update is called once per frame
