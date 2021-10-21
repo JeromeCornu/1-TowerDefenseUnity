f6c180bffc05f9ee62edf00e5d3d6f0edfbb6441
Fouman <guillaume.jacot42@gmail.com>
2021-10-21 18:10:59 +0200

    Wtf

---
+++ Prototype_1/Assets/Scripts/VictoryMenu.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class VictoryMenu : MonoBehaviour
+{
+    public void Start()
+    {
+        
+    }
+    public void GoMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+}
